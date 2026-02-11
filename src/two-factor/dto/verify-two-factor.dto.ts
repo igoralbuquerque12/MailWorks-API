8e0bf54ec2f55793231ffc6a4a4cf5ae2461022f
Igor <igorcostalins2005@gmail.com>
2026-02-11 01:02:42 -0300

    refactor(two-factor): use constants for code length validation

--- src/two-factor/dto/verify-two-factor.dto.ts
+++ src/two-factor/dto/verify-two-factor.dto.ts
@@ -1,10 +1,11 @@
 import { IsEmail, IsString, Length } from 'class-validator';
+import { TWO_FACTOR_CODE_LENGTH } from '../constants/two-factor.constants';
 
 export class VerifyTwoFactorDTO {
   @IsEmail()
   email: string;
 
   @IsString()
-  @Length(6, 6)
+  @Length(TWO_FACTOR_CODE_LENGTH, TWO_FACTOR_CODE_LENGTH)
   code: string;
 }
