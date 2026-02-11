0110874fb6c436681618de7c050cf3a44b531163
Igor <igorcostalins2005@gmail.com>
2026-02-11 01:02:43 -0300

    refactor(two-factor): streamline controller responses and error handling

--- src/two-factor/two-factor.controller.ts
+++ src/two-factor/two-factor.controller.ts
@@ -1,35 +1,20 @@
-import { Controller, Body, Post, Logger, HttpCode } from '@nestjs/common';
+import { Controller, Body, Post, HttpCode } from '@nestjs/common';
 import { TwoFactorService } from './two-factor.service';
 import { SendTwoFactorDTO, VerifyTwoFactorDTO } from './dto';
 
 @Controller('two-factor')
 export class TwoFactorController {
-  private readonly logger = new Logger(TwoFactorController.name);
-
   constructor(private twoFactorService: TwoFactorService) {}
 
   @Post('send')
   @HttpCode(200)
-  send(@Body() body: SendTwoFactorDTO): string {
-    this.twoFactorService.send(body).catch((error) => {
-      this.logger.error('Error with send two factor validation: ', error);
-    });
-    return 'Your two factor autentitcation has been sent!';
+  send(@Body() body: SendTwoFactorDTO) {
+    return this.twoFactorService.send(body);
   }
 
   @Post('verify')
   @HttpCode(200)
-  async verify(@Body() body: VerifyTwoFactorDTO): Promise<boolean> {
-    const verificationCode = await this.twoFactorService
-      .verify(body)
-      .catch((error) => {
-        this.logger.error(
-          'Error with verification two factor service: ',
-          error,
-        );
-        return false;
-      });
-
-    return verificationCode;
+  async verify(@Body() body: VerifyTwoFactorDTO) {
+    return this.twoFactorService.verify(body);
   }
 }
