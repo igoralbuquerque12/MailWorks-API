53618770f8712d836c96da511d76d872784b65f2
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-04-09 23:38:48 -0300

    feat(email): implement SmtpProvider using Nodemailer

---
+++ src/providers/smtp/smtp.provider.ts
@@ -0,0 +1,37 @@
+import { createTransport } from 'nodemailer';
+import {
+  IEmailProvider,
+  EmailPayload,
+} from 'src/common/interfaces/email-provider.interface';
+
+export interface SmtpConfig {
+  host: string;
+  port: number;
+  secure: boolean;
+  user: string;
+  pass: string;
+  fromName?: string;
+}
+
+export class SmtpProvider implements IEmailProvider {
+  constructor(private readonly config: SmtpConfig) {}
+
+  async send(payload: EmailPayload): Promise<void> {
+    const transport = createTransport({
+      host: this.config.host,
+      port: this.config.port,
+      secure: this.config.secure,
+      auth: {
+        user: this.config.user,
+        pass: this.config.pass,
+      },
+    });
+
+    await transport.sendMail({
+      from: `${this.config.fromName || 'MailWorks'} <${this.config.user}>`,
+      to: payload.to,
+      subject: payload.subject,
+      html: `<p>${payload.content}</p>`,
+    });
+  }
+}
