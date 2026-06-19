93c59f1583001832c31fa5e3da01898898773aea
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-06-19 11:12:53 -0300

    refactor(worker): add comment on email worker parallel processing consideration

--- src/lambda/email-worker.handler.ts
+++ src/lambda/email-worker.handler.ts
@@ -23,6 +23,7 @@ export async function handler(event: SQSEvent): Promise<SQSBatchResponse> {
   const service = await getWorkerService();
   const batchItemFailures: Array<{ itemIdentifier: string }> = [];
 
+  // ToDo: processamento aqui é sequencial, isso significa que se cada job demorar 10 seg pra processar, vai demorar 50 seg. Se colocar Promisse.all o processamento fica paralelo, e se o job mais lento demorar 10 seg, o processamento total vai ser de 10 seg, pois os jobs vão processar em paralelo. O risco disso é que se tiver muitos jobs, pode sobrecarregar o banco de dados ou outros recursos. Então tem que pensar na escalabilidade e nos limites do sistema. (mas colocando promisse.all o tempo vai ser praticamente o tempo do job mais lento)
   for (const record of event.Records) {
     try {
       const body = JSON.parse(record.body) as EmailJobMessage;
